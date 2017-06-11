e544a349ae6fd1f967a02968cd0198d1638dd457
lee123lee123@163.com <lee123lee123@163.com>
2017-06-11 13:44:28 +0800

    完成整个项目基础的配置

---
+++ go-push-data-center/src/main/java/com/gopush/datacenter/Application.java
@@ -0,0 +1,21 @@
+package com.gopush.datacenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * go-push
+ *
+ * @类功能说明：
+ * @作者：chenxiangqi
+ * @创建时间：2017/6/11 下午1:28
+ * @VERSION：
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args){
+        SpringApplication.run(Application.class,args);
+    }
+}
---
+++ go-push-node-server/src/main/java/com/gopush/nodeserver/Application.java
@@ -0,0 +1,20 @@
+package com.gopush.nodeserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * go-push
+ *
+ * @类功能说明：
+ * @作者：chenxiangqi
+ * @创建时间：2017/6/11 上午11:43
+ * @VERSION：
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args){
+        SpringApplication.run(Application.class,args);
+    }
+}
