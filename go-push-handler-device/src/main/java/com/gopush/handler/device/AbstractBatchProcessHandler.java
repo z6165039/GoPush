0caf8f0c4e4a5ce997ed8c7cde44d16d024edbee
lee123lee123@163.com <lee123lee123@163.com>
2017-06-13 16:48:05 +0800

    修改pom文件

--- go-push-handler-device/src/main/java/com/gopush/handler/device/AbstractBatchProcessHandler.java
+++ go-push-handler-device/src/main/java/com/gopush/handler/device/AbstractBatchProcessHandler.java
@@ -2,16 +2,15 @@ package com.gopush.handler.device;
 
 
 import lombok.Builder;
-import lombok.Data;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Queue;
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -28,38 +27,125 @@ import java.util.concurrent.atomic.AtomicInteger;
 @Builder
 public abstract class AbstractBatchProcessHandler<T>{
 
-    private final static int maxQueueNum = 10000000;
+    //接收消息的计数
+    private AtomicInteger counter = new AtomicInteger(0);
 
+    //存储多少个子节点处理器
+    private List<InterChildProcessHandler> interChildHandlerList = new ArrayList<>();
+
+
+    /**
+     * 批量处理的定时器延时
+     */
     @Setter
-    private int delay = 1000;
+    private int delay = 1;
 
+    /**
+     * 批量处理的大小
+     */
     @Setter
     private int batchSize = 500;
 
-    protected abstract String getBatchTimerName();
+    /**
+     * 子处理器的个数
+     */
+    @Setter
+    private int childHandlerCount = 1;
 
+
+    /**
+     * 获取定时器执行的name
+     * @return
+     */
+    protected abstract String getBatchExecutorName();
+
+    /**
+     * 处理失败的是否重试
+     * @return
+     */
     protected abstract boolean retryFailure();
 
-    private AtomicInteger counter = new AtomicInteger(0);
 
-    private List<>
 
     @PostConstruct
     public void init(){
-
     }
 
     @PreDestroy
     public void destory(){
+    }
 
+    protected void putMsg(T message){
+        int count = counter.incrementAndGet();
+        if(count == Integer.MAX_VALUE ){
+            counter.set(0);
+        }
+        InterChildProcessHandler processHandler = interChildHandlerList.get( count % childHandlerCount );
+        processHandler.putMsg(message);
     }
 
 
-    private class InterChildHandler {
 
+    private class InterChildProcessHandler {
+
+        //存放消息缓存
+        private Queue<T> queue = new ConcurrentLinkedQueue<T>();
+
+        //handler 索引
         private int index = 0;
 
-        private Queue<T> queue = new ConcurrentLinkedQueue<T>();
+        //是否内部创建的线程池
+        private boolean inBuilder = true;
+
+        /**
+         * 定时器线程池
+         * 支持外部传入，与内部定义
+         */
+        private ScheduledExecutorService  pool;
+
+        /**
+         * 加入未传入线程池，则使用单线程的定时线程，可以设置核心线程数
+         */
+        private int corePoolSize = 1;
+
+        public InterChildProcessHandler(int index){
+            this.index = index;
+        }
+        public InterChildProcessHandler(int index,int corePoolSize){
+            this(index);
+            if (corePoolSize >= 1){
+                this.corePoolSize = corePoolSize;
+            }
+        }
+        public InterChildProcessHandler(int index,ScheduledExecutorService pool){
+            this(index);
+            if(pool != null){
+                this.pool = pool;
+                this.inBuilder = false;
+            }
+        }
+
+        /**
+         * 加入message进入缓存队列
+         * @param message
+         */
+        private void putMsg(T message){
+            queue.add(message);
+        }
+
+        private void start(){
+            if(pool == null){
+                pool = Executors.newScheduledThreadPool(corePoolSize);
+                inBuilder = true;
+            }
+            pool.scheduleWithFixedDelay(() -> {
+                try {
+
+                }catch (Exception e){
+                }
+            },0,delay,TimeUnit.MILLISECONDS);
+
+        }
 
 
 
